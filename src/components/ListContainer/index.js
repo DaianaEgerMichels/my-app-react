01057530ae32f8bc450be038cdd75da24f72e1db
Daiana Eger Michels <daianaem.12@gmail.com>
2021-12-28 15:38:12 -0300

    altera lista

--- src/components/ListContainer/index.js
+++ src/components/ListContainer/index.js
@@ -10,7 +10,7 @@ const ListContainer = ({url,nameClient, nameBank, applicationValue} )=>{
                 <h1>{nameClient}</h1>
             </div>
             <div id="img-money">
-                <img src={url} alt={nameClient}></img>
+                <img src="https://images.pexels.com/photos/3943723/pexels-photo-3943723.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="money"></img>
             </div>
             <div id="details-investiment">
                 <h2>{nameBank}</h2>
