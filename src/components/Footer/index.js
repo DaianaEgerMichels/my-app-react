d098dd26368f1b0e496480ae44649879d50f33c8
Daiana Eger Michels <daianaem.12@gmail.com>
2021-12-28 21:51:55 -0300

    altera footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import '../../App.css';
+import {FaInstagram, FaLinkedin} from "react-icons/fa";
 
 const Footer = ()=> {
     
@@ -7,6 +8,11 @@ const Footer = ()=> {
                 <footer className="footer">
                     <h3>DEV in House - SENAI | ACATE</h3>
                     <p>By: Daiana Eger Michels</p>
+                    <ul className="social_list">
+                            <li>
+                            <a href="https://www.linkedin.com/in/19972309daiana-eger-michels/"><FaLinkedin></FaLinkedin></a>
+                            </li>
+                    </ul>
                 </footer>
         )
     
